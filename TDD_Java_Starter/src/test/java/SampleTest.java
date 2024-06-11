6e943aaf920a6dabd9ef6612abe3d7800739d4c2
Shashank Shekher <shashank.shekher@rea-group.com>
2024-06-11 14:31:45 +0530

    first commit

---
+++ TDD_Java_Starter/src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SampleTest {
+
+    @Test
+    void sampleFailTest() {
+        fail("This test should fail");
+    }
+}
